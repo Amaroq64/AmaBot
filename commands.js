78a1e3946c92dfcc16a92cdcdafaefe13619a40e
Amaroq64 <coolsteve64@yahoo.com>
2025-09-20 17:49:29 -0500

    Validity checks. Added "oversign" command to help.

--- commands.js
+++ commands.js
@@ -3,11 +3,12 @@ global.help =
 "attackdisplay\n" +
 "attackrole(attack_number, role_type, number_of_role)\n" +
 "movenow(creep_name, x, y)\n" +
-"clearcreep(creep_name)";
+"clearcreep(creep_name)\n" +
+"oversign(creep_name)";
 
 global.attack = function(i = false, x = false, y = false, room_name = false)
 {
-	if (i > -1 && x > -1 && y > -1)
+	if (Array.isArray(Memory.attack) && i > -1 && x > -1 && y > -1)
 	{
 		Memory.attack[i].pos.x = x;
 		Memory.attack[i].pos.y = y;
@@ -31,47 +32,73 @@ Object.defineProperty(global, 'attackdisplay',
 {
 	get: function()
 	{
-		let str = '';
-		for (let i = 0; i < Memory.attack.length; i++)
+		if (Array.isArray(Memory.attack))
 		{
-			str = str + "Attack " + i + ": " + Memory.attack[i].pos.x + ", " + Memory.attack[i].pos.y;
-			if (i < Memory.attack.length - 1)
+			let str = '';
+			for (let i = 0; i < Memory.attack.length; i++)
 			{
-				str = str + "\n"
+				str = str + "Attack " + i + ": " + Memory.attack[i].pos.x + ", " + Memory.attack[i].pos.y;
+				if (i < Memory.attack.length - 1)
+				{
+					str = str + "\n"
+				}
 			}
+			return str;
 		}
-		return str;
+		else
+		{
+			return false;
+		}
 	}
 });
 
-/*global.attackdisplay = function()
-{
-	for (let i = 0; i < Memory.attack.length; i++)
-	{
-		console.log("Attack " + i + ": " + Memory.attack[i].pos.x + ", " + Memory.attack[i].pos.y);
-	}
-	return true;
-}*/
-
 global.attackrole = function(i, role, r)
 {
-	return Memory.attack[i].ideal[role] = r;
+	if (Array.isArray(Memory.attack))
+	{
+		return Memory.attack[i].ideal[role] = r;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 global.movenow = function(creep_name, x, y)
 {
-	Memory.creeps[creep_name].movenow = Game.creeps[creep_name].pos.findPathTo(x, y);
-	return true;
+	if (Memory.creeps[creep_name])
+	{
+		Memory.creeps[creep_name].movenow = Game.creeps[creep_name].pos.findPathTo(x, y);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
 }
 
 global.clearcreep = function(creep_name)
 {
-	return Memory.creeps[creep_name].movenow = [];
+	if (Memory.creeps[creep_name])
+	{
+		return Memory.creeps[creep_name].movenow = [];
+	}
+	else
+	{
+		return false;
+	}
 }
 
 global.oversign = function(creep_name)
 {
-	return Game.creeps[creep_name].signController(Game.creeps[creep_name].room.controller, require('claim').oversignature);
+	if (Memory.creeps[creep_name])
+	{
+		return Game.creeps[creep_name].signController(Game.creeps[creep_name].room.controller, require('claim').oversignature);
+	}
+	else
+	{
+		return false;
+	}
 }
 
 /*global.backupAttack = function(n)
