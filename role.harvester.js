feb3466db93e927205edb528abf69b7d708eb0f3
Amaroq64 <coolsteve64@yahoo.com>
2025-12-17 01:39:32 -0600

    Minor tweak.

--- role.harvester.js
+++ role.harvester.js
@@ -2,7 +2,7 @@ var roleHarvester =
 {
 	run: function(creep, s)
 	{
-	    if(creep.harvest(Game.getObjectById(Memory.rooms[creep.room.name].sources[s].id)) == ERR_NOT_IN_RANGE)
+		if(creep.harvest(Game.getObjectById(Memory.rooms[creep.room.name].sources[s].id)) === ERR_NOT_IN_RANGE)
 		{
 			return true;	//We need to move to the source.
 		}
