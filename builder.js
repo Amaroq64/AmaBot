3cc87d4169e0b0f43733ec16665a24a11a2961f3
Amaroq64 <coolsteve64@yahoo.com>
2026-01-14 15:13:35 -0600

    Hybrid role added. (It harvests and upgrades.)

--- builder.js
+++ builder.js
@@ -22,7 +22,10 @@ var builder =
 			for (i = 0; i < Memory.rooms[room_name].sources.length; i++)
 			{
 				//It's not enough to emergency on a missing transport, we have to do it on a missing harvester too. But we want the harvester to be big enough too.
-				if (Memory.rooms[room_name].sources[i].creeps.mtransport.length == 0 || (Memory.rooms[room_name].sources[i].creeps.harvester.length == 0 && Game.rooms[room_name].energyAvailable >= 550))
+				if (((Memory.rooms[room_name].sources[i].ideal.mtransport && Memory.rooms[room_name].sources[i].creeps.mtransport.length === 0)
+					|| (Memory.rooms[room_name].sources[i].ideal.harvester && Memory.rooms[room_name].sources[i].creeps.harvester.length === 0)
+					|| (Memory.rooms[room_name].sources[i].ideal.hybrid && Memory.rooms[room_name].sources[i].creeps.hybrid.length === 0))
+					&& (Game.rooms[room_name].energyAvailable >= 550 || Game.rooms[room_name].find(FIND_MY_CREEPS).length <= 1))	//If we have nothing, we have a true emergency.
 					//Now that we're calculating maximum energy based on completed structures, we shouldn't need to emergency on a missing extension.
 					//|| myextensions.length < CONTROLLER_STRUCTURES.extension[Game.rooms[room_name].controller.level])
 				{
@@ -31,7 +34,7 @@ var builder =
 			}
 
 			//We should only build if our energy is full. Or if it's an emergency.
-			if ((Game.rooms[room_name].energyAvailable < calculate.maximumEnergy(room_name)) && !emergency)
+			if ((calculate.currentEnergy(room_name) < calculate.maximumEnergy(room_name)) && !emergency)
 			{
 				//We don't have the amount of energy our room level could max out at. Skip it for now.
 				continue;
@@ -69,7 +72,7 @@ var builder =
 			let anyharvest = true;
 			for (let i = 0; i < Memory.rooms[room_name].sources.length; i++)
 			{
-				if (Memory.rooms[room_name].sources[i].creeps.mtransport.length - anticipate.sources[i].mtransport == 0)
+				if (Memory.rooms[room_name].sources[i].creeps.mtransport.length - anticipate.sources[i].mtransport === 0)
 				{
 					anyharvest = false;
 				}
@@ -93,6 +96,12 @@ var builder =
 			{
 				let needcount = {};
 
+				//Establish our priority roles by taking the first role of each source.
+				for (let i = 0; i < Memory.rooms[room_name].sources.length; i++ )
+				{
+					needcount[Object.keys(Memory.rooms[room_name].sources[i].creeps)[0]] = null;
+				}
+
 				for (let i = 0; i < Memory.rooms[room_name].sources.length; i++ )	//Iterate over the sources.
 				{
 					//console.log(i);
@@ -100,12 +109,20 @@ var builder =
 					{
 						//For now, we're building a list of each role and how many each source needs of it. Iterating that after is the easiest way.
 						//Otherwise we would have to hack together something by iterating just one source's roles and then assuming the other sources share the same roles.
-						if (i == 0)
+						if (!needcount[creep_role])
 						{
-							needcount[creep_role] = [];	//While on the first source, none of these roles will be initialized as arrays yet.
+							needcount[creep_role] = new Array(Memory.rooms[room_name].sources.length).fill(0);
 						}
+
 						//How many of this role is needed for each source?
-						needcount[creep_role][i] = Memory.rooms[room_name].sources[i].ideal[creep_role] - Memory.rooms[room_name].sources[i].creeps[creep_role].length + anticipate.sources[i][creep_role];
+						if (Memory.rooms[room_name].sources[i].ideal[creep_role] === undefined)
+						{
+							needcount[creep_role][i] = 0;
+						}
+						else
+						{
+							needcount[creep_role][i] = Memory.rooms[room_name].sources[i].ideal[creep_role] - Memory.rooms[room_name].sources[i].creeps[creep_role].length + anticipate.sources[i][creep_role];
+						}
 					}
 				}
 
@@ -118,8 +135,13 @@ var builder =
 					//However, it's better for our room if we get one harvester and one mtransport on our first source as soon as possible.
 					for (let i = 0; i < needcount[creep_role].length; i++)
 					{
+						if (Memory.rooms[room_name].sources[i].ideal[creep_role] === undefined)
+						{
+							continue;
+						}
+
 						//If we're about to build an additional harvester while the previous one doesn't have an mtransport, skip the entire harvester role.
-						if (creep_role == "harvester" && i > 0 && Memory.rooms[room_name].sources[i - 1].creeps.mtransport.length == 0)
+						if ((creep_role === 'harvester' || creep_role === 'hybrid') && i > 0 && Memory.rooms[room_name].sources[i - 1].creeps.mtransport.length === 0)
 						{
 							break;
 						}
@@ -160,7 +182,7 @@ var builder =
 			}
 
 			//Fabricate a name for the new creep.
-			let name = spawn.name.slice(0, 3) + spawn.name.slice(6) + role.charAt(0).toUpperCase() + role.slice(1) + need + "_" + Game.time.toString();
+			let name = spawn.name.slice(0, 3) + spawn.name.slice(require('builder').newSpawn.basename.length) + role.charAt(0).toUpperCase() + role.slice(1) + need + "_" + Game.time.toString();
 
 			let options = {memory: {movenow: []}, directions: null};	//Define the options object for the spawner.
 
@@ -168,76 +190,95 @@ var builder =
 			let direction;
 			switch(role)
 			{
-				case "harvester":	//It could be helpful to store this to make checks easier.
+				case 'custodian':
+				{
+					let labs = require('labs');
+					//labs.request(name, [RESOURCE_ZYNTHIUM_OXIDE, RESOURCE_UTRIUM_ALKALIDE, RESOURCE_CATALYZED_KEANIUM_ACID]);
+					options.memory.mission = require('role.custodian').missions.length - 1;	//It needs to select a mission. By default we start with getting boosted.
+					options.memory.harv = body[role](Game.rooms[room_name].energyAvailable).reduce(calculate.reduceManyWork, 0) * 2;
+					options.memory.t = 0;
+					options.memory.lmission = [];	//It should track what missions it has already performed.
+					options.memory.direction = false;
+					//options.memory.ldirection = false;	//If it makes a pit stop, it needs to go back where it came from.
+					options.memory.path = 0;	//The mine path is the initial path from the spawn.
+					options.memory.s = -1;	//We'll probably get boosted first, which is a room-wide path.
+					options.memory.d_path = 0;	//Desired path. It needs to get to the right path for the job from its current path.
+					options.memory.d_s = 0;	//Sometimes it needs to switch to another source.
+					options.memory.target = {x: null, y: null};	//Not sure if we need this.
+					direction = [Memory.rooms[room_name].spawns[spawn_index].dir.minedir[0]];
+					break;
+				}
+				case 'harvester':
+				case 'hybrid':
 				{
 					options.memory.direction = false;
 					options.memory.path = 0;
-					options.memory.target = {};
-					options.memory.target.x = Memory.rooms[room_name].sources[need].mfat.slice(0, 1)[0].x;
-					options.memory.target.y = Memory.rooms[room_name].sources[need].mfat.slice(0, 1)[0].y;
+					options.memory.target =
+					{
+						x: Memory.rooms[room_name].sources[need].mfat.slice(0, 1)[0].x,
+						y: Memory.rooms[room_name].sources[need].mfat.slice(0, 1)[0].y
+					};
 					direction = [Memory.rooms[room_name].spawns[spawn_index].dir.minedir[need]];	//It goes to a source.
 					break;
 				}
-				case "mtransport":	//We might not have a container yet. Store the desired position.
+				case 'mtransport':
 				{
 					options.memory.direction = false;
 					options.memory.path = 0;
-					options.memory.target = {};
-					options.memory.target.x = Memory.rooms[room_name].sources[need].mine.slice(-1)[0].x;
-					options.memory.target.y = Memory.rooms[room_name].sources[need].mine.slice(-1)[0].y;
-					//options.memory.dtarget = {};
-					//options.memory.dtarget.x = Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need].slice(-1)[0].x;
-					//options.memory.dtarget.y = Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need].slice(-1)[0].y;
+					options.memory.target =
+					{
+						x: Memory.rooms[room_name].sources[need].mine.slice(-1)[0].x,
+						y: Memory.rooms[room_name].sources[need].mine.slice(-1)[0].y
+					};
 					options.memory.utrip = false;
-					//options.memory.return = false;
 					options.memory.dtrip = false;
 					direction = [Memory.rooms[room_name].spawns[spawn_index].dir.minedir[need]];	//It goes to a source.
 					break;
 				}
-				//case "ubuilder":
-				case "upgrader":	//It could be helpful to store this to make checks easier.
+				case 'upgrader':
 				{
 					options.memory.direction = false;
 					options.memory.path = 11;
-					options.memory.target = {};
-					options.memory.target.x = Memory.rooms[room_name].upgrade.slice(-1)[0].x;
-					options.memory.target.y = Memory.rooms[room_name].upgrade.slice(-1)[0].y;
+					options.memory.target =
+					{
+						x: Memory.rooms[room_name].upgrade.slice(-1)[0].x,
+						y: Memory.rooms[room_name].upgrade.slice(-1)[0].y
+					};
 					direction = [Memory.rooms[room_name].spawns[spawn_index].dir.upgradedir];	//It goes to the controller.
 					break;
 				}
-				case "utransport":	//We might not have a container yet. Store the desired position.
+				case 'utransport':	//We might not have a container yet. Store the desired position.
 				{
 					//Our target is the last step in the upgrader's path, since that's where it will be waiting.
 					options.memory.direction = Memory.rooms[room_name].sources[need].minedir2;
-					options.memory.path = 3;
-					options.memory.target = {};
-					options.memory.target.x = Memory.rooms[room_name].upgrade.slice(-1)[0].x;
-					options.memory.target.y = Memory.rooms[room_name].upgrade.slice(-1)[0].y;
-					//options.memory.return = false;
+					options.memory.path = 0;
+					options.memory.target =
+					{
+						x: Memory.rooms[room_name].upgrade.slice(-1)[0].x,
+						y: Memory.rooms[room_name].upgrade.slice(-1)[0].y
+					};
 					direction = [Memory.rooms[room_name].spawns[spawn_index].dir.minedir[need]];	//It goes to a source initially.
 					//This one needs temporary instructions to get to a source. Then it loops between the source and the controller upgrader on its own.
 					options.memory.movenow = Memory.rooms[room_name].sources[need].mclean.u;
 					//options.memory.movenow.pop();
 					break;
 				}
-				case "builder":
+				case 'builder':
 				{
 					options.memory.direction = Memory.rooms[room_name].sources[need].minedir2;
 					options.memory.path = 0;
-					options.memory.target = {};	//We need to flip the utrip bool every time we visit the source.
-					options.memory.target.x = Memory.rooms[room_name].sources[need].mine.slice(-1)[0].x;
-					options.memory.target.y = Memory.rooms[room_name].sources[need].mine.slice(-1)[0].y;
-					options.memory.dtarget = {};
-					if (Memory.rooms[room_name].sources[need].defpaths)
+					options.memory.target =
 					{
-						if (Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need])
-						{
-							options.memory.dtarget.x = Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need].slice(-1)[0].x;
-							options.memory.dtarget.y = Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need].slice(-1)[0].y;
-						}
+						x: Memory.rooms[room_name].sources[need].mine.slice(-1)[0].x,
+						y: Memory.rooms[room_name].sources[need].mine.slice(-1)[0].y
+					};
+					options.memory.dtarget = {};
+					if (Memory.rooms[room_name].sources[need].defpaths && Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need])
+					{
+						options.memory.dtarget.x = Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need].slice(-1)[0].x;
+						options.memory.dtarget.y = Memory.rooms[room_name].sources[need].defpaths[Memory.rooms[room_name].defense.need].slice(-1)[0].y;
 					}
 					options.memory.utrip = false;	//This breaks movement if we try to be clever by starting it as true.
-					//options.memory.return = true;
 					options.memory.dtrip = false;
 					options.memory.destination = false;
 					options.memory.need = Memory.rooms[room_name].defense.need;
@@ -247,7 +288,7 @@ var builder =
 					//options.memory.movenow.pop();
 					break;
 				}
-				case "dbuilder":
+				case 'dbuilder':
 				{
 					//We should go to a different defense every time we spawn.
 					//Only go to unsafe ones though.
@@ -255,7 +296,7 @@ var builder =
 					let exits = Memory.rooms[room_name].exits
 					for (defense.need++; defense.need <= exits.length; defense.need++)
 					{
-						if (defense.need == exits.length)
+						if (defense.need === exits.length)
 						{
 							defense.need = 0;
 						}
@@ -297,8 +338,6 @@ var builder =
 						options.memory.dtarget.y = options.memory.target.y;
 					}
 					options.memory.movenow = Memory.rooms[room_name].sources[shortestchosen].mclean.d[defense.need];
-					//options.memory.movenow = [];
-					//options.memory.return = false;
 					options.memory.dtrip = true;
 					options.memory.need = defense.need;
 					options.memory.s = shortestchosen;
@@ -318,7 +357,6 @@ var builder =
 
 			//Assign the direction into our options to be passed to the spawner.
 			options.directions = direction;
-			//console.log(JSON.stringify(direction));
 
 			//Sort the extensions and spawners for efficient energy usage. Don't do this if there are extension construction sites.
 			if (myextensions.length === Memory.rooms[room_name].sources.reduce(calculate.sourcereducer.idealextensions, 0))
@@ -327,7 +365,7 @@ var builder =
 			}
 
 			//Build what's needed.
-			console.log("Building " + name + ".");
+			console.log('Building ' + name + '.');
 
 			//Options are defined before the direction switch so we can assign our transport targets too.
 			//If there are no construction sites, our builder should be a minbuilder, but still considered a builder in all other ways.
@@ -336,8 +374,7 @@ var builder =
 			{
 				case OK:
 				{
-					//console.log("Attempting to save " + name + ".");
-					if (need !== "")
+					if (need !== '')
 					{
 						Memory.rooms[room_name].sources[need].creeps[role].push(name);
 					}
@@ -349,37 +386,43 @@ var builder =
 				}
 				case ERR_NOT_ENOUGH_ENERGY:
 				{
-					console.log("Not Enough Energy.");
+					console.log('Not Enough Energy.');
+					//We probably got here due to an edge case where the room is out of energy but the third spawn (which we're not using) has some energy.
+					//So try again without the restraint on energy sources.
+					delete options.energyStructures;
+					console.log(spawn.spawnCreep(body[[role, 'minbuilder'][+(role === 'builder' && Game.rooms[room_name].find(FIND_MY_CONSTRUCTION_SITES).length === 0)]](Game.rooms[room_name].energyAvailable), name, options));
+					console.log(room_name + ' ' + role + ' ' + Game.rooms[room_name].energyAvailable);
+					console.log(JSON.stringify(body[[role, 'minbuilder'][+(role === 'builder' && Game.rooms[room_name].find(FIND_MY_CONSTRUCTION_SITES).length === 0)]](Game.rooms[room_name].energyAvailable)));
 					break;
 				}
 				case ERR_NOT_OWNER:
 				{
-					console.log("You Are Not The Owner.");
+					console.log('You Are Not The Owner.');
 					break
 				}
 				case ERR_INVALID_ARGS:
 				{
-					console.log("Invalid Arguments.");
+					console.log('Invalid Arguments.');
 					break
 				}
 				case ERR_NAME_EXISTS:
 				{
-					console.log("Name Exists.");
+					console.log('Name Exists.');
 					break
 				}
 				case ERR_BUSY:
 				{
-					console.log("Busy.");
+					console.log('Busy.');
 					break
 				}
 				case ERR_RCL_NOT_ENOUGH:
 				{
-					console.log("RCL Not High Enough.");
+					console.log('RCL Not High Enough.');
 					break
 				}
 				default:
 				{
-					console.log("Error building creep.");
+					console.log('Error building creep.');
 				}
 			}
 		}
@@ -464,7 +507,7 @@ var builder =
 		{
 			name = builder.newSpawn.basename + (1 + ((Object.keys(Memory.rooms).length + a) * 3));
 		}
-		console.log("Spawn named " + name);
+		console.log('Spawn named ' + name);
 		return name;
 	},
 
