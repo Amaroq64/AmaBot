9829e4bc7eb35975383098523b4bbab004bceab8
Amaroq64 <coolsteve64@yahoo.com>
2026-02-07 18:04:59 -0600

    Updated some old double equals to triple equals.

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,9 +2,9 @@ var roleUpgrader =
 {
 	run: function(creep)
 	{
-		if (creep.carry.energy == 0)
+		if (creep.carry.energy === 0)
 		{
-			if (creep.pickup(creep.pos.findInRange(FIND_DROPPED_RESOURCES, 1)[0]) != OK)	//We're picking up off the ground first.
+			if (creep.pickup(creep.pos.findInRange(FIND_DROPPED_RESOURCES, 1)[0]) !== OK)	//We're picking up off the ground first.
 			{
 				creep.withdraw(creep.pos.lookFor(LOOK_STRUCTURES)[0], RESOURCE_ENERGY);	//This should be the container.
 			}
@@ -13,7 +13,7 @@ var roleUpgrader =
 		//This could potentially have undesired behavior. If we want it to move closer than maximum range, it will never get there unless it has energy. But it will never get energy unless something dies by it.
 		if(creep.upgradeController(creep.room.controller) === ERR_NOT_IN_RANGE || !creep.pos.isEqualTo(creep.memory.target.x, creep.memory.target.y))
 		{
-			return true;	//We need to move to the source.
+			return true;	//We need to move to the upgrader.
 		}
 		else
 		{
